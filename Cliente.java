9737d6f9fb6ca74d4ef4d2acf93b517395cc1df2
Nikkous <132166127+SoftwareFactoryGN@users.noreply.github.com>
2023-05-20 17:06:19 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,35 +1,24 @@
-
+package negocio;
+import java.io.Serializable;
 import java.util.*;
 
-/**
- * 
- */
 public class Cliente {
 
-    /**
-     * Default constructor
-     */
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private Contacto contacto;
+    
     public Cliente() {
     }
+    
+    public Cliente(String nombre, String apellido, String dni, Contacto contacto) {
+    	this.nombre = nombre;
+    	this.apellido = apellido;
+    	this.dni = dni;
+    	this.contacto = contacto;
+    }
 
-    /**
-     * 
-     */
-    private String nombre;
 
-    /**
-     * 
-     */
-    private String apellido;
-
-    /**
-     * 
-     */
-    private String dni;
-
-    /**
-     * 
-     */
-    private Contacto contacto;
 
 }
